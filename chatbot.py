b66d460da5a789dda9a1fbe06df38f975eac9ff7
Bharadwaj Swaminathan <bharad.swami@gmail.com>
2019-11-09 16:59:04 -0800

    Updated all_results to start numbering at 1

--- chatbot.py
+++ chatbot.py
@@ -95,7 +95,7 @@ def all_results(text):
 	addresses = []
 	for result in all_results:
 		addresses.append(address(result))
-	return list(enumerate(addresses))
+	return list(enumerate(addresses, 1))
 
 
 # company_names = pregen_company_names(data)
